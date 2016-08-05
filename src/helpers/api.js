fbde4478c411523b8e97f300d71da955e4174b9c
Guilherme Toti <guilhermetoti@gmail.com>
2016-08-04 23:29:23 -0300

    build

--- src/helpers/api.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = 'https://api.github.com';
-
-
-export const getUserData = (username) => {
-  return fetch(`${baseUrl}/users/${username}`)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-};
